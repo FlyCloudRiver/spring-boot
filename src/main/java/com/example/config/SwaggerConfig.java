4c9af7437898c4c4ebed8844f7c37bd87851af70
jiangyunfei <948742327@qq.com>
2019-03-14 10:14:44 +0800

    swagger描述

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -5,7 +5,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,7 +35,19 @@ public class SwaggerConfig {
     }
    @Bean
    public Docket swaggerSpringMvcPlugin() {
-       return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+       return new Docket(DocumentationType.SWAGGER_2)
+               .apiInfo(apiInfo())
+               .select()
+               .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+               .build();
    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restful风格，http://XX")
+                .termsOfServiceUrl("http://XXx")
+                .version("1.0")
+                .build();
+    }
 
 }
