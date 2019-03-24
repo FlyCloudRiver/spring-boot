fc06111a4b80a0e4993435e963fb8755492814ab
jiang <948742327@qq.com>
2019-03-24 10:59:48 +0800

    更改注解

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 public interface UserMapper {
     /*测试*/
     User selectUserById(Integer userId);
