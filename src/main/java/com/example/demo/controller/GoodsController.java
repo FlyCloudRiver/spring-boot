96dd7ea7fb9e99e705f7e7cc9ae2657346258a1c
jiang <948742327@qq.com>
2019-03-28 15:49:07 +0800

    添加商品分类实体

---
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: 江云飞
+ * Date:   2019/3/28
+ */
+
+
+@RestController
+@Api(description = "商品" )   //swagger
+@RequestMapping("/goods")
+public class GoodsController {
+
+
+}
